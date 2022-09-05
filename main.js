7489a89dc2e9ce7b1dfec676a195776e89475457
DQN <nguyenhcm1999@gmail.com>
2022-09-05 20:39:04 +0700

    5/9

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // app được gán bằng IIFE vì bản chất của IIFE là 1 hàm chạy được ngay nên
 // return cái gì trong hàm sẽ được gán = app
 
@@ -22,7 +23,6 @@ var app = (function(){
         }
     }
 })() 
-
 
 var number = (function(a,b){ 
     return a + b
@@ -93,7 +93,7 @@ console.log(d)
 function makeCounter(){
     let counter = 0
     function increase(){
-        return ++ counter
+        return ++counter
     }
 
     return increase
@@ -194,3 +194,160 @@ let calc = {
 // calc.read()
 console.log(calc.sum())
 
+
+
+// # Closure
+// Là một hàm có thể ghi nhớ nơi nó được tạo và truy cập được biến ở bên ngoài phạm
+// vi của nó
+
+// ## Ứng dụng
+// - Viết code ngắn gọn hơn
+// - Biểu diễn, ứng dụng tính Private trong OOP
+
+// ## Tóm tắt
+
+// ## Lưu ý
+// - Biến được tham chiếu (refer) trong closure sẽ không được xóa khỏi bộ nhớ
+// khi hàm cha thực thi xong
+// - Các khái niệm Javascript nâng cao rất dễ gây nhầm lẫn
+
+function createLogger(namespace) {
+    function logger(message){
+        console.log(`[${namespace}] ${message}`)
+    }
+    return logger
+}
+
+// ============= App =================
+
+// Register.js
+
+const infoLogger = createLogger('Info')
+infoLogger('Bắt đầu gửi mail')
+infoLogger(' Gửi mail lỗi lần 1, thử gửi lại')
+
+// ForgotPassword.js
+
+const errorLogger = createLogger('Error')
+errorLogger('Email không tồn tại trong DB')
+errorLogger(' Gửi mail lỗi lần 1, thử gửi lại')
+
+
+
+function creatStorage(key) {
+    const store = JSON.parse(localStorage.getItem(key)) ?? {}
+
+    const save = function(){
+        localStorage.setItem(key, JSON.stringify(store))
+    }
+
+    const storage = {
+        get(key) {
+            return store[key]
+        },
+        set(key,value) {
+// nó ghi nhớ phạm vi được tạo ra nên nó có quyền truy cập vào biến store ở phạm vi
+// bên ngoài
+            store[key] = value 
+            save()
+        },
+        remove(key) {
+            delete store[key]
+            save()
+        }
+
+    }
+    
+    return storage
+}
+
+// Profile.js
+const profileSetting = creatStorage('profile_setting')
+
+console.log(profileSetting.get('fullName'))
+
+profileSetting.set('fullName', 'Son Dang')
+profileSetting.set('age', 20)
+profileSetting.set('address', 'Ha Noi')
+
+function fff(x) {
+    x++;
+    return function () {
+        console.log(++x);
+    };
+}
+
+fff(1)();
+
+let x = fff(1);
+x()
+x()
+
+
+let fullName1 = "Nguyen Van A"
+
+{
+    let fullName1 = "Nguyen Van B"
+    {
+        {
+            // console.log(fullName1) Cannot access 'fullName1' before initialization
+
+            let fullName1 = " Nguyen Van C"
+        }
+    }
+}
+
+var counter1 = makeCounter()
+
+console.log(counter1(1))
+
+function makeCounter() {
+    let counter = 0
+    return increase
+    // hàm increase được hoist đưa lên đầu phạm vi của nó
+    function increase(){
+        return ++counter
+    }
+}
+
+// var nhất định nghĩa biến lên trên đầu, var được hỗ trợ hoisting còn 
+// let/ const khi được hoisted không được tạo giá trị và được đưa vào temporal dead zone
+// r =10
+// console.log(r) // sẽ báo lỗi vì lúc này r nằm trong temporal dead zone
+// let r 
+
+// Strict Mode - Nghiêm ngặt
+// Báo lỗi hoặc ngăn chặn khi sử dụng những đoạn mã không an toàn hay dễ gây nhầm lẫn
+
+// Cách sử dụng
+// "use strict" thêm vào đầu file.js thì sẽ báo lỗi fullNametest và age2
+// Thêm "use strict" vào ngay sau thẻ mở <script>
+// Thêm "use strict" vào đầu phạm vi hàm
+fullNametest = "Nguyen Van A"
+function testFunc(){
+    // "use strict" // Ví dụ thêm "use strict"
+    age2 = 18
+}
+testFunc()
+console.log(fullNametest)
+console.log(age2)
+
+// fetch('https://123jsonplaceholder.typicode.com/no-such-page')
+// .then((res) => { 
+//     console.log(res.status); // 404
+//     return res.json();
+// })
+// .then(data => console.log('Success:', data))
+// .catch(error => console.log('Error:', error));
+
+function sum(...args) { // args is the name for the array
+    let sum = 0;
+    console.log((typeof args).length)
+    args.forEach((arg) => sum += arg)
+  
+    return sum;
+  }
+  
+  console.log( sum(1) ); // 1
+  console.log( sum(1, 2) ); // 3
+  console.log( sum(1, 2, 3) ); // 6
